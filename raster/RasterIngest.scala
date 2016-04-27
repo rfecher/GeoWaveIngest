99ab1d523092f11bfa602102548861ca18d41e41
James McClain <jmcclain@azavea.com>
2016-04-27 11:51:36 -0400

    Disable Pyramiding

--- raster/RasterIngest.scala
+++ raster/RasterIngest.scala
@@ -64,7 +64,7 @@ object RasterIngest {
       log.error("Invalid arguments, expected: zookeepers, accumuloInstance, accumuloUser, accumuloPass, geowaveNamespace, rasterFile");
       System.exit(-1)
     }
-    val coverageName = "anything, not empty" // This must not be empty
+    val coverageName = "coverageName" // This must not be empty
     val metadata = new java.util.HashMap[String, String]()
     val image = getGridCoverage2D(args(5))
 
@@ -72,7 +72,7 @@ object RasterIngest {
     val dataStore = getGeowaveDataStore(basicOperations)
     val index = createSpatialIndex
     // https://ngageoint.github.io/geowave/apidocs/mil/nga/giat/geowave/adapter/raster/adapter/RasterDataAdapter.html
-    val adapter = new RasterDataAdapter(coverageName, metadata, image, 256, true)
+    val adapter = new RasterDataAdapter(coverageName, metadata, image, 256, false)
 
     val indexWriter = dataStore.createWriter(adapter, index).asInstanceOf[IndexWriter[GridCoverage]]
 
