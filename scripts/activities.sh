676b44bd57a5539bd2b5dcef5f338df32851ae1e
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-07-23 16:44:56 -0400

    Fix "Integration Test" Script

--- scripts/activities.sh
+++ scripts/activities.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp /poke-jar/raster-poke-assembly-0.jar com.example.raster.RasterIngest geodocker-zookeeper gis root GisPwd gwRaster /rasters/TC_NG_Baghdad_IQ_Geo.tif
+java -cp /poke-jar/raster-poke-assembly-0.jar com.daystrom_data_concepts.raster.RasterIngest geodocker-zookeeper gis root GisPwd gwRaster /rasters/TC_NG_Baghdad_IQ_Geo.tif
 /spark/bin/spark-submit --master='spark://geodocker-spark-master:7077' --conf 'spark.driver.memory=1G' --class com.example.raster.RasterDisgorge /peek-jar/raster-peek-assembly-0.jar geodocker-zookeeper gis root GisPwd gwRaster
