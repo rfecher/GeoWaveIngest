9a8e294500b04c1746c1ed92bb9535a0f5e86bab
James W. McClain <jmcclain@daystrom-data-concepts.com>
2016-10-08 23:46:56 -0400

    Use Histogram Equalization To Make Layer Viewable

--- demo/src/main/scala/com/azavea/geotrellis/geowave/Demo.scala
+++ demo/src/main/scala/com/azavea/geotrellis/geowave/Demo.scala
@@ -28,58 +28,16 @@ object Demo {
   /**
     * Dump a layer to disk.
     */
-  def dump(rdd: RDD[(SpatialKey, Tile)] with Metadata[TileLayerMetadata[SpatialKey]]) = {
+  def dump(rdd: RDD[(SpatialKey, Tile)] with Metadata[TileLayerMetadata[SpatialKey]], stem: String) = {
     val mt = rdd.metadata.mapTransform
 
     rdd.collect.foreach({ case (k, v) =>
       val extent = mt(k)
       val pr = ProjectedRaster(Raster(v, extent), LatLng)
       val gc = pr.toGridCoverage2D
-      val writer = new GeoTiffWriter(new java.io.File(s"/tmp/tif/${System.currentTimeMillis}.tif"))
+      val writer = new GeoTiffWriter(new java.io.File(s"/tmp/tif/${stem}-${System.currentTimeMillis}.tif"))
       writer.write(gc, Array.empty[GeneralParameterValue])
     })
-  }
-
-  /**
-    * Project a value (from some raw raster) into a viewable range
-    * by use of a list of quantiles.
-    */
-  def projection(quantiles: Array[Double])(x: Double): Int = {
-    val i = java.util.Arrays.binarySearch(quantiles, x)
-    if (i < 0) ((-1 * i) - 1); else i
-  }
-
-  /**
-    * Project all of the values of the pixel values of a key+tile RDD
-    * into a viewable range.
-    */
-  def viewable(rdd: RDD[(SpatialKey, Tile)] with Metadata[TileLayerMetadata[SpatialKey]]) = {
-    val histogram = rdd.histogram(512)
-    val breaks = histogram.quantileBreaks(256)
-    val p = projection(breaks)(_)
-
-    ContextRDD(
-      rdd.map({case (key, tile) =>
-        val newTile = UByteArrayTile.empty(tile.cols, tile.rows, UByteUserDefinedNoDataCellType(0))
-
-        var i = 0; while (i < tile.cols) {
-          var j = 0; while (j < tile.rows) {
-            newTile.set(i, j, p(tile.getDouble(i, j)))
-            j += 1
-          }
-          i += 1
-        }
-
-        (key, newTile.asInstanceOf[Tile])
-      }),
-      TileLayerMetadata(
-        UByteConstantNoDataCellType,
-        rdd.metadata.layout,
-        rdd.metadata.extent,
-        rdd.metadata.crs,
-        rdd.metadata.bounds
-      )
-    )
   }
 
   def main(args: Array[String]) : Unit = {
@@ -104,7 +62,7 @@ object Demo {
     val accumuloUser = args(2)
     val accumuloPass = args(3)
     val geowaveNamespace = args(4)
-    val CACHE_DIR = args(5)
+    val hdfsUri = args(5)
     val layerName = args(6)
     val zoomLevel = args(7).toInt
 
@@ -117,8 +75,8 @@ object Demo {
       val inLayerId = LayerId(layerName, zoomLevel)
       val subset = Extent(-87.1875, 34.43409789359469, -78.15673828125, 39.87601941962116)
 
-      require(HadoopAttributeStore(CACHE_DIR).layerExists(inLayerId))
-      HadoopLayerReader(CACHE_DIR)
+      require(HadoopAttributeStore(hdfsUri).layerExists(inLayerId))
+      HadoopLayerReader(hdfsUri)
         // .read[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](inLayerId)
         .query[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](inLayerId)
         .where(Intersects(subset))
@@ -129,7 +87,7 @@ object Demo {
     layerWriter.write(LayerId(layerName + "-raw", 0), rdd1)
 
     logger.info("Writing viewable layer into GeoWave")
-    layerWriter.write(LayerId(layerName + "-viewable", 0), viewable(rdd1))
+    layerWriter.write(LayerId(layerName + "-viewable", 0), rdd1.equalize)
 
     val tier =
       (1 to 33)
@@ -148,10 +106,10 @@ object Demo {
     logger.info("Reading raw layer from GeoWave")
     val rdd2 = layerReader.read[SpatialKey, Tile, TileLayerMetadata[SpatialKey]](LayerId(layerName + "-raw", tier))
 
-    logger.info("Writing viewable hillshade layer into GeoWave")
+    logger.info("Writing hillshade layer into GeoWave")
     layerWriter.write(
       LayerId(layerName + "-hillshade", 0),
-      viewable(rdd2.hillshade()),
+      rdd2.hillshade(),
       tier // Here, it is assumed that tier and precision coincide
     )
   }
